0684ba4fd3e3ca3b899f22fe2f9adb46831b1c63
Jordane Lew <jml168@uclive.ac.nz>
2015-03-11 11:21:18 +1300

    Fixed packages. Make sure to build and run before pushing.

---
+++ src/test/java/seng302/group2/project/team/person/PersonTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seng302.group2.project.team.person;
+
+/**
+ *
+ * @author Jordane Lew (jml168)
+ */
+public class PersonTest
+{
+    
+}
