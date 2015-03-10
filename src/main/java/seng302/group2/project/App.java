75f052fe376a000ce2e4765ae8c0dda8f59dbe42
David Moseley <drm127@uclive.ac.nz>
2015-03-11 12:09:07 +1300

    Removed duplicate Scenes.java

--- src/main/java/seng302/group2/project/App.java
+++ src/main/java/seng302/group2/project/App.java
@@ -6,6 +6,7 @@ package seng302.group2.project;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.stage.Stage;
+import seng302.group2.project.scenes.Scenes;
 
 
 /**
