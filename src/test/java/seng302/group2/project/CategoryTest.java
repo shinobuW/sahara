e792bd7dfae4c9a871aa5533c09f3cd7980cb0d5
Codie Stevens <cvs20@uclive.ac.nz>
2015-03-18 16:36:40 +1300

    Wrote JUnit test for CategoryTest

---
+++ src/test/java/seng302/group2/project/CategoryTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seng302.group2.project;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * A series of tests relating to Category
+ * @author Codie Stevens (cvs20)
+ */
+public class CategoryTest extends TestCase
+{
+    /**
+     * Create the test case
+     * @param testName name of the test case
+     */
+    public CategoryTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CategoryTest.class );
+    }
+
+    /**
+     * A simple test for the Category constructors
+     */
+    public void testCategoryConstructors()
+    {
+        Category cat = new Category("aName");
+        Assert.assertEquals("aName", cat.toString());
+    }
+}
