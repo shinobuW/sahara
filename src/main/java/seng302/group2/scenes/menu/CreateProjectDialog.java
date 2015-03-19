1488a17b0de1118c481e845c6aca9a298ff6a7ec
David <David@DavidMoseleyPC>
2015-03-19 21:40:21 +1300

    Added a project creation dialog class, with some basic validation.

---
+++ src/main/java/seng302/group2/scenes/menu/CreateProjectDialog.java
@@ -0,0 +1,89 @@
+package seng302.group2.scenes.menu;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import org.controlsfx.control.action.Action;
+import org.controlsfx.dialog.Dialog;
+import org.controlsfx.dialog.Dialogs;
+import seng302.group2.App;
+import seng302.group2.project.Project;
+
+/**
+ *
+ * @author David Moseley drm127
+ */
+public class CreateProjectDialog 
+{
+    public static void show()
+    {
+        Dialog dialog = new Dialog(null, "Create New Project");
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        
+        TextField shortNameField = new TextField();
+        TextField longNameField = new TextField();
+        TextArea descriptionField = new TextArea();
+        descriptionField.setPrefRowCount(15);
+        descriptionField.setWrapText(true);
+        Button btnCreate = new Button("Create");
+        
+        grid.add(new Label("Short Name: "), 0, 0);
+        grid.add(shortNameField, 1, 0);
+        grid.add(new Label("Longer Name: "), 0, 1);
+        grid.add(longNameField, 1, 1);
+        grid.add(new Label("Project Description: "), 0, 2);
+        grid.add(descriptionField, 1, 2);
+        grid.add(btnCreate, 1, 3);
+        
+        btnCreate.setOnAction((event) ->
+            {
+                String shortName = shortNameField.getText();
+                String longName = longNameField.getText();
+                String description = descriptionField.getText();
+                
+                int valid = validation(shortName, longName, description);
+                
+                if (valid == 1)
+                {
+                    Project project = new Project(shortName, longName, description);
+                    System.out.println("Testing" + shortName);
+                    App.currentProject = project;
+                    dialog.hide();
+                }
+                else
+                {
+                    event.consume();
+                };
+            });
+        
+        dialog.setResizable(false);
+        dialog.setIconifiable(false);
+        dialog.setContent(grid);
+        dialog.show();
+    }
+    
+    private static int validation(String shortName, String longName, String description)
+    {
+        int valid = 0;
+        
+        if (shortName.equals("") || longName.equals("") || description.equals(""))
+        {
+            Dialogs.create()
+                .title("Need more info!")
+                .message("You must enter a short name, long name, and"
+                        + " description for your new project.")
+                .showInformation();
+        }
+        else
+        {
+            valid = 1;
+        }
+        
+        return valid;
+        
+    }
+}
