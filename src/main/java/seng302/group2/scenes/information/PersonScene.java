540e80cc2e005a75ec20738293d035812c11467b
Cameron Williams <crw73@uclive.ac.nz>
2015-03-20 16:32:33 +1300

    Information now displaying for the Person and Project Displays.

--- src/main/java/seng302/group2/scenes/information/PersonScene.java
+++ src/main/java/seng302/group2/scenes/information/PersonScene.java
@@ -7,8 +7,14 @@ package seng302.group2.scenes.information;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import seng302.group2.App;
+import static seng302.group2.App.informationGrid;
+import static seng302.group2.App.selectedTreeItem;
+import seng302.group2.project.team.person.Person;
 
 /**
  * A class for displaying the Person Scene
@@ -17,16 +23,26 @@ import seng302.group2.App;
 public class PersonScene
 {
     
-    
     public static GridPane getPersonScene()
     {
         
-        App.informationGrid = new GridPane();
+        Person currentPerson = (Person) selectedTreeItem.getValue();
+        informationGrid = new GridPane();
         
-        App.informationGrid.setAlignment(Pos.CENTER);
-        App.informationGrid.setHgap(10);
-        App.informationGrid.setVgap(10);
-        App.informationGrid.setPadding(new Insets(25,25,25,25));
+        informationGrid.setAlignment(Pos.TOP_LEFT);
+        informationGrid.setHgap(10);
+        informationGrid.setVgap(10);
+        informationGrid.setPadding(new Insets(25,25,25,25));
+        Label title = new Label(currentPerson.getFirstName() + " " + currentPerson.getLastName());
+        title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
+        
+        informationGrid.add(title, 0, 0);
+        informationGrid.add(new Label("Short Name: " + currentPerson.getShortName()), 0, 1);
+        informationGrid.add(new Label("Email Address: " + currentPerson.getEmail()), 0, 2);
+        informationGrid.add(new Label("Birth Date: " + currentPerson.getBirthDate().toString()), 
+                0, 3);
+        informationGrid.add(new Label("Description: " + currentPerson.getDescription()), 0, 4);
+        
         
         return App.informationGrid;
     }
--- src/main/java/seng302/group2/scenes/information/ProjectScene.java
+++ src/main/java/seng302/group2/scenes/information/ProjectScene.java
@@ -7,14 +7,14 @@ package seng302.group2.scenes.information;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import seng302.group2.App;
-import seng302.group2.project.team.person.Person;
+import static seng302.group2.App.informationGrid;
+import static seng302.group2.App.selectedTreeItem;
+import seng302.group2.project.Project;
 
 /**
  * A class for displaying the Project Scene
@@ -26,36 +26,20 @@ public class ProjectScene
     public static GridPane getProjectScene()
     {
         
-        // <editor-fold defaultstate="collapsed" desc="Information Grid">
+        Project currentProject = (Project) selectedTreeItem.getValue();
+        informationGrid = new GridPane();
         
-        // Set up a grid pane for displaying information in the window
-        App.informationGrid = new GridPane();
+        informationGrid.setAlignment(Pos.TOP_LEFT);
+        informationGrid.setHgap(10);
+        informationGrid.setVgap(10);
+        informationGrid.setPadding(new Insets(25,25,25,25));
+        Label title = new Label(currentProject.getLongName());
+        title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
         
-        App.informationGrid.setAlignment(Pos.CENTER);
-        App.informationGrid.setHgap(10);
-        App.informationGrid.setVgap(10);
-        App.informationGrid.setPadding(new Insets(25,25,25,25));
-
-        // Adds a title to the grid
-        Text scenetitle = new Text("Welcome");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        App.informationGrid.add(scenetitle, 0, 0, 2, 1);
-        // At top-left (0, 0), spanning 2 columns and 1 row
-
-        // A button to switch to the demo scene
-        Button btn = new Button("Make Test Person");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
-        App.informationGrid.add(hbBtn, 1, 4);
-        
-        //The button event handler
-        btn.setOnAction((event) ->
-            {
-                App.currentProject.addPerson(new Person());
-            });
-        
-        // </editor-fold>
+        informationGrid.add(title, 0, 0);
+        informationGrid.add(new Label("Short Name: " + currentProject.getShortName()), 0, 1);
+        informationGrid.add(new Label("Email Address: " + currentProject.getDescription()), 0, 2);
+ 
         return App.informationGrid;
     }
  
