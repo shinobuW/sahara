79362c8b7fbf1dfdfe6b382c98975e99e09b1948
Jordane Lew <jml168@uclive.ac.nz>
2015-04-02 14:43:22 +1300

    Update on required field class to take an error message and display it below the textfield. Methods for showing and hiding the error are implemented.

--- src/main/java/seng302/group2/scenes/control/RequiredField.java
+++ src/main/java/seng302/group2/scenes/control/RequiredField.java
@@ -2,22 +2,63 @@ package seng302.group2.scenes.control;
 
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 /**
  * Created by Jordane on 24/03/2015.
  */
-public class RequiredField extends HBox
+public class RequiredField extends VBox
 {
+    String errorMessage = "";
+    TextField errorMessageText = new TextField();
+
     /**
-     * Creates a required label HBox containing a Label with an appended red asterisk
+     * Creates a required label HBox inside of the VBox containing a Label with an appended red
+     * asterisk.
      * @param node The node field that is required
      */
     public RequiredField(Node node)
     {
+        this.errorMessageText.setText(errorMessage);
+
+        HBox detail = new HBox();
         Label aster = new Label("*");
         aster.setTextFill(Color.web("#ff0000"));
-        this.getChildren().addAll(node, aster);
+        detail.getChildren().addAll(node, aster);
+
+        this.getChildren().add(node);
+    }
+
+
+    /**
+     * Shows the error field
+     */
+    public void showErrorField()
+    {
+        this.getChildren().remove(errorMessageText);    // Ensure that it is not shown already
+        this.getChildren().add(errorMessageText);
+    }
+
+
+    /**
+     * Shows the error field with the with the given text
+     * @param errorMessage The error message to show
+     */
+    public void showErrorField(String errorMessage)
+    {
+        this.errorMessageText.setText(errorMessage);
+        showErrorField();
+    }
+
+
+    /**
+     * Hides the error field
+     */
+    public void hideErrorField()
+    {
+        this.getChildren().remove(errorMessageText);
     }
 }
