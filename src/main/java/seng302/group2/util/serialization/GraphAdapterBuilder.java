80c359e789df7b18aaff77bdc3a20221e55a1991
Jordane Lew <jml168@uclive.ac.nz>
2015-04-13 19:15:10 +1200

    Moved the GraphAdapterBuilder to the util.serialization package.

--- src/main/java/seng302/group2/util/serialization/GraphAdapterBuilder.java
+++ src/main/java/seng302/group2/util/serialization/GraphAdapterBuilder.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.google.gson.graph;
+package seng302.group2.util.serialization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
