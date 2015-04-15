f0c178f4915e2a58949184676712e5a3febcd819
Jordane Lew <jml168@uclive.ac.nz>
2015-04-15 23:09:19 +1200

    Tidy up with enums and added simple deletion method body

--- src/main/java/seng302/group2/scenes/contextmenu/ElementTreeContextMenu.java
+++ src/main/java/seng302/group2/scenes/contextmenu/ElementTreeContextMenu.java
@@ -4,96 +4,165 @@ import java.text.MessageFormat;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import org.controlsfx.control.action.Action;
+import org.controlsfx.dialog.Dialog;
 import org.controlsfx.dialog.Dialogs;
 import seng302.group2.App;
 import seng302.group2.Global;
+import seng302.group2.project.Project;
 import seng302.group2.project.skills.Skill;
 import seng302.group2.project.team.Team;
 import seng302.group2.project.team.person.Person;
 import seng302.group2.scenes.MainScene;
 import seng302.group2.scenes.information.PersonEditScene;
+import seng302.group2.scenes.information.ProjectEditScene;
 import seng302.group2.scenes.information.SkillEditScene;
 import seng302.group2.scenes.information.TeamEditScene;
 
 /**
  *ContextMenu class for instances of Person, Skill and Team
- * @author swi67
+ * @author swi67, jml168
  */
+@SuppressWarnings("deprecation")
 public class ElementTreeContextMenu extends ContextMenu
 {
-    /** Constructor. Sets the event for contextMenu buttons
+    /**
+     * An enumeration of the categories selected.
+     * Each category my have different menu items associated.
+     */
+    private enum Categories
+    {
+        PERSON,
+        SKILL,
+        TEAM,
+        PROJECT,
+        OTHER  // For anything else, or unresolved.
+    }
+
+
+    /**
+     * Constructor. Sets the event for contextMenu buttons
      */
     public ElementTreeContextMenu()
     {
-        MenuItem deleteItem = new MenuItem("Delete...");
-        MenuItem editItem = new MenuItem("Edit...");
+        MenuItem deleteItem = new MenuItem("Delete");
+        MenuItem editItem = new MenuItem("Edit");
         
-        String categoryName = "";
-        
-        if (Global.selectedTreeItem.getValue().getClass() == Person.class)
-        {
-            categoryName = "Person";
-        }
-        else if (Global.selectedTreeItem.getValue().getClass() == Skill.class)
-        {
-            categoryName = "Skill";
-        }
-        else if (Global.selectedTreeItem.getValue().getClass() == Team.class)
-        {
-            categoryName = "Team";
-        }
-        final String className = categoryName;
-        
+        Categories selectedCategory = getSelectedCategory();
+
+        final Categories finalSelectedCategory = selectedCategory;  // for use in lamda expr.
+
         //"Edit" button event
         editItem.setOnAction(e ->
             {
-                showEditScene(className);
+                showEditScene(finalSelectedCategory);
             });
         
         //"Delete" button event
         deleteItem.setOnAction(e -> 
             {
-                showDeleteDialog(className);
+                showDeleteDialog(finalSelectedCategory);
             });
 
         this.getItems().addAll(editItem, deleteItem);
     }
-    
-    /** Displays the edit scene for given element
-     * @param categoryName Type of Category
+
+
+    /**
+     * Gets the category of the selected tree item.
+     * @return An enum value of the selected tree item's category
      */
-    public static void showEditScene(String categoryName)
+    private Categories getSelectedCategory()
+    {
+        Categories selectedCategory = Categories.OTHER;
+        if (Global.selectedTreeItem.getValue().getClass() == Person.class)
+        {
+            selectedCategory = Categories.PERSON;
+        }
+        else if (Global.selectedTreeItem.getValue().getClass() == Skill.class)
+        {
+            selectedCategory = Categories.SKILL;
+        }
+        else if (Global.selectedTreeItem.getValue().getClass() == Team.class)
+        {
+            selectedCategory = Categories.TEAM;
+        }
+        else if (Global.selectedTreeItem.getValue().getClass() == Project.class)
+        {
+            selectedCategory = Categories.PROJECT;
+        }
+
+        return selectedCategory;
+    }
+
+
+    /**
+     * Displays the edit scene for given element
+     * @param category Type of Category
+     */
+    public static void showEditScene(Categories category)
     {
         App.content.getChildren().remove(MainScene.informationGrid);
         
-        switch (categoryName)
+        switch (category)
         {
-            case "Person":           
+            case PERSON:
                 PersonEditScene.getPersonEditScene((Person) Global.selectedTreeItem.getValue());
                 break;
-            case "Skill":
+            case SKILL:
                 SkillEditScene.getSkillEditScene();
                 break;
-            case "Teams":
+            case TEAM:
                 TeamEditScene.getTeamEditScene();
                 break;
+            case PROJECT:
+                ProjectEditScene.getProjectEditScene();
+            case OTHER:
+                System.out.println("The category was not correctly recognized");
+                break;
             default:
+                System.out.println("The category was not set");
                 break;
         }
         App.content.getChildren().add(MainScene.informationGrid);
     }
-    
-    /**Shows a confirm dialog box for element deletion
-     * @param categoryName Type of Category
+
+
+    /**
+     * Shows a confirm dialog box for element deletion
+     * @param category Type of Category
      */
-    public static void showDeleteDialog(String categoryName)
+    public static void showDeleteDialog(Categories category)
     {
         Action response = Dialogs.create()
             .title("Delete item?")
-            .message(MessageFormat.format("Do you want to delete {0}: {1}", categoryName, 
-                    Global.selectedTreeItem.getValue().getClass().toString()))
+            .message(MessageFormat.format("Are you sure you want to delete {0}",
+                    Global.selectedTreeItem.getValue().toString() + "?"))
             .showConfirm();
-        
-        //To Do for deletion story
+
+        if (response == Dialog.ACTION_YES || response == Dialog.ACTION_OK)
+        {
+            switch (category)
+            {
+                case PERSON:
+                    Global.currentProject.getPeople().remove(Global.selectedTreeItem.getValue());
+                    break;
+                case PROJECT:
+                    //TODO: Check if valid after project/workspace refactoring and uncomment
+                    //Global.currentProject.getProjects().remove(Global.selectedTreeItem.getValue());
+                    break;
+                case TEAM:
+                    Global.currentProject.getTeams().remove(Global.selectedTreeItem.getValue());
+                    break;
+                case SKILL:
+                    Global.currentProject.getSkills().remove(Global.selectedTreeItem.getValue());
+                    break;
+                case OTHER:
+                    System.out.println("Can't delete unknown selected class");
+                    break;
+                default:
+                    System.out.println("Did not identify the class of object to delete");
+                    break;
+            }
+        }
     }
 }
