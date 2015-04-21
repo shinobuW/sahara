146e8ac188d732cfa931c16891cdf46f26ff83a5
Shinobu <Shinobu@shinobu-laptop>
2015-04-21 13:14:53 +1200

    New custom ComboBox Field Class - implemented in ReleaseEditScene

---
+++ src/main/java/seng302/group2/scenes/control/CustomComboBox.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seng302.group2.scenes.control;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author Shinobu
+ */
+public class CustomComboBox extends VBox
+{
+    String errorMessage = "";
+    Label errorMessageText = new Label();
+    ObservableList<String> options = FXCollections.observableArrayList();
+    ComboBox comboBox = new ComboBox(options);
+    
+    /**
+     * Creates a required label HBox inside of the VBox containing a Label with an appended red
+     * asterisk.
+     * @param name The node field that is required
+     */
+    public CustomComboBox(String name)
+    {
+        this.errorMessageText.setText(errorMessage);
+
+        HBox labelBox = new HBox();
+        labelBox.setPrefWidth(165);
+        labelBox.spacingProperty().setValue(0);
+                
+        labelBox.getChildren().addAll(new Label(name));
+        
+        HBox entry = new HBox();
+        entry.setPrefWidth(175);
+        entry.getChildren().addAll(labelBox, comboBox);
+        
+        comboBox.setStyle("-fx-pref-width: 135;");
+
+        this.getChildren().add(entry);
+    }
+    
+    public void showErrorField()
+    {
+        comboBox.setStyle("-fx-border-color: red;");
+        this.getChildren().remove(errorMessageText);    // Ensure that it is not shown already
+        this.getChildren().add(errorMessageText);
+    }
+    
+    public void addToComboBox(String item)
+    {
+       this.options.add(item); 
+    }
+}
