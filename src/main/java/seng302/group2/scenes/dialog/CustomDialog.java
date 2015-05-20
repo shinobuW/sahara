7e56ecb7f136f2c0e72c65fba0d1d39395790713
Jordane Lew <jordane.lew@outlook.com>
2015-05-20 19:58:10 +1200

    A custom dialogs class for standardized JavaFX alerts

---
+++ src/main/java/seng302/group2/scenes/dialog/CustomDialog.java
@@ -0,0 +1,44 @@
+package seng302.group2.scenes.dialog;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
+
+/**
+ * Created by Jordane on 20/05/2015.
+ */
+public class CustomDialog
+{
+    /**
+     * Shows a standard dialog with the given text and severity
+     * @param title The title string (dialog caption)
+     * @param message The dialog content
+     * @param severity The dialog severity
+     */
+    public static void showDialog(String title, String message, Alert.AlertType severity)
+    {
+        Alert alert = new Alert(severity);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+
+    /**
+     * Shows a confirmation box with the given text
+     * @param title The title string (dialog caption)
+     * @param question The confirmation content/question
+     * @return The button option that was chosen
+     */
+    public static ButtonType showConfirmation(String title, String question)
+    {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Look, a Confirmation Dialog");
+        alert.setContentText("Are you ok with this?");
+
+        return alert.showAndWait().get();
+    }
+}
