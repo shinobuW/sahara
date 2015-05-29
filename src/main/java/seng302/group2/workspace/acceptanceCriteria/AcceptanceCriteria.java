442e37c445042e98a4e40d66bc3f68112fdafc3d
Shinobu Williams <swi67@uclive.ac.nz>
2015-05-30 01:23:57 +1200

    Created a class for Acceptance Criteria. Includes getters and setters for its properties

---
+++ src/main/java/seng302/group2/workspace/acceptanceCriteria/AcceptanceCriteria.java
@@ -0,0 +1,78 @@
+package seng302.group2.workspace.acceptanceCriteria;
+
+import seng302.group2.workspace.story.Story;
+
+/**
+ * Created by Shinobu on 30/05/2015.
+ */
+public class AcceptanceCriteria
+{
+    private String text;
+    private acState state;
+    private Story story;
+    private enum acState
+    {
+        DONE,
+        ACCEPTED,
+        UNACCEPTED
+    }
+
+
+    /**
+     * Sets the text of the AC
+     * @param text text to set
+     */
+    public void setText(String text)
+    {
+        this.text = text;
+    }
+
+
+    /**
+     * Sets the state of the AC to done, accepted or unaccepted
+     * @param state the state to set
+     */
+    public void setAcState(acState state)
+    {
+        this.state = state;
+    }
+
+
+    /**
+     * Sets the story the AC belongs to
+     * @param story to set
+     */
+    public void setStory (Story story)
+    {
+        this.story = story;
+    }
+
+
+    /**
+     * Get the text
+     */
+    public String getText()
+    {
+        return this.text;
+    }
+
+
+    /**
+     * Gets the state
+     */
+    public acState getState()
+    {
+        return this.state;
+    }
+
+
+    /**
+     * Gets the story the AC belongs to
+     */
+    public Story getStory()
+    {
+        return this.story;
+    }
+}
+
+
