9269b21f333d98bda2e6988b47fa39e3929da5f9
jml168 <jml168@uclive.ac.nz>
2015-07-03 11:27:34 +1200

    Added a size() method in the CumulativeCommand class that may be helpful in future implementation. Added simple cumulative command tests that check whether adding items works as expected, and the execution and undo of CumulativeCommands are performed correctly. Also added a constructor that takes in an initial collection of sub-commands.

--- src/main/java/seng302/group2/util/undoredo/CumulativeCommand.java
+++ src/main/java/seng302/group2/util/undoredo/CumulativeCommand.java
@@ -2,24 +2,47 @@ package seng302.group2.util.undoredo;
 
 import seng302.group2.scenes.listdisplay.TreeViewItem;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
+ * A command that nests a collection of other commands such that they can be
  * Created by jml168 on 2/07/15.
  */
 public class CumulativeCommand implements Command {
 
-    List<Command> commands = new ArrayList<>();
+    Collection<Command> commands = new LinkedList<>();
 
     /**
-     * Adds a command to the list of cumulative commands
+     * The default constructor for CumulativeCommands, has no initial sub-commands
+     */
+    public CumulativeCommand() {
+        super();
+    }
+
+    /**
+     * A constructor that takes in an initial collection of sub-commands
+     * @param commands The initial collection of sub-commands
+     */
+    public CumulativeCommand(Collection<Command> commands) {
+        super();
+        this.commands.addAll(commands);
+    }
+
+    /**
+     * Adds a command to the collection of cumulative commands
      * @param command The command to add
      * @return The result of adding the command to the cumulative commands collection
      */
     public boolean add(Command command) {
         return commands.add(command);
+    }
+
+    /**
+     * Gets the number of sub-commands inside the cumulative command
+     * @return The size of the list of sub-commands
+     */
+    public int size() {
+        return commands.size();
     }
 
     @Override
---
+++ src/test/java/seng302/group2/util/undoredo/CumulativeCommandTest.java
@@ -0,0 +1,117 @@
+package seng302.group2.util.undoredo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import seng302.group2.Global;
+import seng302.group2.scenes.listdisplay.TreeViewItem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+/**
+ * A series of tests for cumulative commands
+ * Created by Jordane on 2/07/2015.
+ */
+public class CumulativeCommandTest {
+
+    @Test
+    public void testAdd() throws Exception {
+        CumulativeCommand command = new CumulativeCommand();
+        TestObject t1 = new TestObject("original1");
+        TestObject t2 = new TestObject("original2");
+        command.add(new TestCommand(t1, "executed1"));
+        command.add(new TestCommand(t2, "executed2"));
+
+        Assert.assertEquals(2, command.size());
+    }
+
+    @Test
+    public void testCollectionConstructor() {
+        Global.commandManager.clear();
+
+        TestObject t1 = new TestObject("original1");
+        TestObject t2 = new TestObject("original2");
+        List<Command> commands = new ArrayList<Command>() {{
+            add(new TestCommand(t1, "executed1"));
+            add(new TestCommand(t2, "executed2"));
+        }};
+
+        CumulativeCommand command = new CumulativeCommand(commands);
+
+        Assert.assertEquals(2, command.size());
+    }
+
+    @Test
+    public void testUndoRedo() throws Exception {
+        Global.commandManager.clear();
+
+        TestObject t1 = new TestObject("original1");
+        TestObject t2 = new TestObject("original2");
+        List<Command> commands = new ArrayList<Command>() {{
+            add(new TestCommand(t1, "executed1"));
+            add(new TestCommand(t2, "executed2"));
+        }};
+
+        CumulativeCommand command = new CumulativeCommand(commands);
+
+        Assert.assertEquals("original1", t1.testString);
+        Assert.assertEquals("original2", t2.testString);
+
+        Global.commandManager.executeCommand(command);
+
+        Assert.assertEquals("executed1", t1.testString);
+        Assert.assertEquals("executed2", t2.testString);
+
+        Global.commandManager.undo();
+
+        Assert.assertEquals("original1", t1.testString);
+        Assert.assertEquals("original2", t2.testString);
+
+        Global.commandManager.redo();
+
+        Assert.assertEquals("executed1", t1.testString);
+        Assert.assertEquals("executed2", t2.testString);
+    }
+
+
+
+
+
+    private class TestObject {
+        public String testString;
+
+        public TestObject(String string) {
+            this.testString = string;
+        }
+    }
+
+    private class TestCommand implements Command {
+        TestObject obj;
+        String newName;
+        String oldName;
+
+        public TestCommand(TestObject obj, String newName) {
+            this.obj = obj;
+            this.newName = newName;
+            this.oldName = obj.testString;
+        }
+
+        @Override
+        public void execute() {
+            obj.testString = newName;
+        }
+
+        @Override
+        public void undo() {
+            obj.testString = oldName;
+        }
+
+        @Override
+        public boolean map(Set<TreeViewItem> stateObjects) {
+            return true;
+        }
+    }
+}
