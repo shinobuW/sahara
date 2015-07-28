631b91fb326a8a54b61f3633ee58f7c8f353a6c1
Shinobu Williams <swi67@uclive.ac.nz>
2015-07-28 20:24:20 +1200

    Changed the dialogs to controlsFX to javaFX

--- src/main/java/seng302/group2/util/config/ConfigLoader.java
+++ src/main/java/seng302/group2/util/config/ConfigLoader.java
@@ -2,7 +2,11 @@ package seng302.group2.util.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.controlsfx.dialog.Dialogs;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
 import java.io.*;
 
@@ -12,7 +16,7 @@ import java.io.*;
  * http://docs.oracle.com/javase/8/docs/technotes/guides/preferences/index.html
  * Created by Jordane on 13/04/2015.
  */
-@SuppressWarnings("deprecation")
+
 public class ConfigLoader {
     private static String configLocation = "./config";
 
@@ -30,11 +34,42 @@ public class ConfigLoader {
             writer.close();
         }
         catch (IOException e) {
-            Dialogs.create()
-                    .title("Error Saving")
-                    .message("An error occurred while trying to save Sahara's configuration "
-                            + "settings")
-                    .showException(e);
+//            Dialogs.create()
+//                    .title("Error Saving")
+//                    .message("An error occurred while trying to save Sahara's configuration "
+//                            + "settings")
+//                    .showException(e);
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Exception");
+            alert.setHeaderText("Error Saving");
+            alert.setContentText("An error occurred while trying to save Sahara's configuration settings");
+// Create expandable Exception.
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            String exceptionText = sw.toString();
+
+            Label label = new Label("The exception stacktrace was:");
+
+            TextArea textArea = new TextArea(exceptionText);
+            textArea.setEditable(false);
+            textArea.setWrapText(true);
+
+            textArea.setMaxWidth(Double.MAX_VALUE);
+            textArea.setMaxHeight(Double.MAX_VALUE);
+            GridPane.setVgrow(textArea, Priority.ALWAYS);
+            GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+            GridPane expContent = new GridPane();
+            expContent.setMaxWidth(Double.MAX_VALUE);
+            expContent.add(label, 0, 0);
+            expContent.add(textArea, 0, 1);
+
+            // Set expandable Exception into the dialog pane.
+            alert.getDialogPane().setExpandableContent(expContent);
+
+            alert.showAndWait();
         }
     }
 
