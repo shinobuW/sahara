d626976cd8299667db7b10f0e2b3eb8b8fc36d00
Shinobu Williams <swi67@uclive.ac.nz>
2015-08-02 16:44:19 +1200

    Highlight colour defined in SearchableControl to be able to used by all controls that implement it

--- src/main/java/seng302/group2/scenes/control/search/SearchableControl.java
+++ src/main/java/seng302/group2/scenes/control/search/SearchableControl.java
@@ -1,8 +1,13 @@
 package seng302.group2.scenes.control.search;
 
+import javafx.scene.paint.Color;
+import seng302.group2.workspace.project.story.Story;
+
 /**
  * Created by jml168 on 31/07/15.
  */
 public interface SearchableControl {
+    public static String highlightColour = Story.toRGBCode(Color.rgb(255, 255, 0, 0.5));
+
     boolean query(String query);
 }
