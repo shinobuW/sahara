4dc31114905b6f3a87ae8f372d817ba484719633
Cameron Williams <Darzolak@hotmail.com>
2015-08-06 12:23:28 +1200

    Added StartTime, EndTime, Duration to the Logging Table under Tasks. Also added in fields for adding a person, StartTime and EndTime.
    Currently Adding a person Works along with a default duration, but StartTime and EndTime not yet coming up in the Table.

--- src/main/java/seng302/group2/scenes/information/project/story/task/TaskLoggingTab.java
+++ src/main/java/seng302/group2/scenes/information/project/story/task/TaskLoggingTab.java
@@ -8,21 +8,31 @@ package seng302.group2.scenes.information.project.story.task;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import javafx.collections.FXCollections;
+import static javafx.collections.FXCollections.observableArrayList;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import seng302.group2.App;
+import seng302.group2.scenes.control.CustomComboBox;
+import seng302.group2.scenes.control.CustomDatePicker;
 import seng302.group2.scenes.control.RequiredField;
 import seng302.group2.scenes.control.search.SearchableControl;
 import seng302.group2.scenes.control.search.SearchableTab;
@@ -33,6 +43,7 @@ import static seng302.group2.util.validation.ShortNameValidator.validateShortNam
 import seng302.group2.workspace.person.Person;
 import seng302.group2.workspace.project.story.tasks.Log;
 import seng302.group2.workspace.project.story.tasks.Task;
+import seng302.group2.workspace.team.Team;
 
 /**
  *
@@ -40,10 +51,9 @@ import seng302.group2.workspace.project.story.tasks.Task;
  */
 public class TaskLoggingTab extends SearchableTab {
     List<SearchableControl> searchControls = new ArrayList<>();
-    //TODO Refactor into searchable controls and populate
 
     /**
-     * Constructor for the Task Info Tab
+     * Constructor for the Task Logging Tab
      *
      * @param currentTask The currently selected Task
      */
@@ -70,9 +80,24 @@ public class TaskLoggingTab extends SearchableTab {
         loggerCol.setCellValueFactory(new PropertyValueFactory<Task, Person>("logger"));
         loggerCol.prefWidthProperty().bind(taskTable.widthProperty()
                 .subtract(2).divide(100).multiply(60));
+        
+        TableColumn startTimeCol = new TableColumn("Start Time");
+        startTimeCol.setCellValueFactory(new PropertyValueFactory<Task, LocalDate>("startTime"));
+        startTimeCol.prefWidthProperty().bind(taskTable.widthProperty()
+                .subtract(2).divide(100).multiply(60));
+        
+        TableColumn endTimeCol = new TableColumn("End Time");
+        endTimeCol.setCellValueFactory(new PropertyValueFactory<Task, LocalDate>("endTime"));
+        endTimeCol.prefWidthProperty().bind(taskTable.widthProperty()
+                .subtract(2).divide(100).multiply(60));
+        
+        TableColumn durationCol = new TableColumn("Duration");
+        durationCol.setCellValueFactory(new PropertyValueFactory<Task, Long>("duration"));
+        durationCol.prefWidthProperty().bind(taskTable.widthProperty()
+                .subtract(2).divide(100).multiply(60));
 
         taskTable.setItems(data);
-        TableColumn[] columns = {loggerCol};
+        TableColumn[] columns = {loggerCol, startTimeCol, endTimeCol, durationCol};
         taskTable.getColumns().setAll(columns);
 
         // Listener to disable columns being movable
@@ -97,28 +122,108 @@ public class TaskLoggingTab extends SearchableTab {
             });
 
         VBox addTaskBox = new VBox(10);
-        SearchableText task = new SearchableText("Add Quick Tasks:", "-fx-font-weight: bold;");
+        
+        HBox buttons = new HBox(10);
+        buttons.setAlignment(Pos.BOTTOM_RIGHT);
+        Button addButton = new Button("Add");
+        Button deleteButton = new Button("Delete");
+        buttons.getChildren().addAll(addButton, deleteButton);
+        
+        
+        
+        HBox newLogFields = new HBox(35);
+        final ComboBox<Person> personComboBox = new ComboBox<>(observableArrayList());
+        CustomDatePicker startDatePicker = new CustomDatePicker("Start Date", true);
+        CustomDatePicker endDatePicker = new CustomDatePicker("End Date", false);
 
+        startDatePicker.getDatePicker().setStyle("-fx-pref-width: 200;");
+        endDatePicker.getDatePicker().setStyle("-fx-pref-width: 200;");
+        personComboBox.setStyle("-fx-pref-width: 250;");
 
-        RequiredField shortNameCustomField = new RequiredField("Short Name:");
-        Button btnAdd = new Button("Add");
-        btnAdd.setDisable(true);
+        personComboBox.prefWidthProperty().bind(taskTable.widthProperty()
+                .subtract(3).divide(100).multiply(30));
+        startDatePicker.prefWidthProperty().bind(taskTable.widthProperty()
+                .subtract(3).divide(100).multiply(30));
+        endDatePicker.prefWidthProperty().bind(taskTable.widthProperty()
+                .subtract(3).divide(100).multiply(30));
+        newLogFields.getChildren().addAll(personComboBox,
+                startDatePicker, endDatePicker);
 
-        addTaskBox.getChildren().addAll(task, shortNameCustomField, btnAdd);
+        personComboBox.setOnMouseClicked(event -> {
+                personComboBox.getItems().clear();
+                //TODO not going to work on tasks without a story
+                for (Team team : currentTask.getStory().getBacklog().getProject().getCurrentTeams()) {
+                    for (Person person : team.getPeople()) {
+                        personComboBox.getItems().add(person);
+                    }
+                }
+            });
 
+        addButton.setOnAction((event) -> {
+                startDatePicker.hideErrorField();
+                if (personComboBox.getValue() != null && startDatePicker.getValue() != null) {
+                    LocalDate endDate = endDatePicker.getValue();
+                    LocalDate startDate = startDatePicker.getValue();
+                    Person selectedPerson = null;
+
+                    selectedPerson = personComboBox.getValue();
+
+                    if (true) {
+                        Log newLog = new Log(currentTask, selectedPerson, 90, startDate);
+                        currentTask.add(newLog);
+                        App.refreshMainScene(); 
+                    }
+                    else {
+                        
+                        event.consume();
+                    }
+                }
+                else {
+                    if (personComboBox.getValue() == null) {
+                        //personComboBox.showErrorField("Please select a team");
+                        event.consume();
+                    }
+                    if (startDatePicker.getValue() == null) {
+                        startDatePicker.showErrorField("Please select a date");
+                        event.consume();
+                    }
+                }
+            });
+
+        deleteButton.setOnAction((event) -> {
+                Log selectedLog = taskTable.getSelectionModel().getSelectedItem();
+                if (selectedLog != null) {
+
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                    alert.setTitle("Delete");
+                    alert.setHeaderText("Delete Allocation?");
+                    alert.setContentText("Do you really want to delete this allocation?");
+                    alert.getDialogPane().setStyle(" -fx-max-width:450; -fx-max-height: 100px; -fx-pref-width: 450px; "
+                            + "-fx-pref-height: 100px;");
+
+                    ButtonType buttonTypeYes = new ButtonType("Yes");
+                    ButtonType buttonTypeNo = new ButtonType("No");
+
+                    alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
+                    Optional<ButtonType> result  = alert.showAndWait();
+
+                    if (result.get() == buttonTypeYes) {
+                        //selectedLog.delete();
+                    }
+                    else if (result.get() == buttonTypeNo) {
+                        event.consume();
+                    }
+                }
+            });
+
+        
         basicInfoPane.getChildren().add(taskTable);
         basicInfoPane.getChildren().add(btnView);
-        basicInfoPane.getChildren().add(addTaskBox);
+        basicInfoPane.getChildren().add(newLogFields);
+        basicInfoPane.getChildren().add(buttons);
 
        
-        btnAdd.setOnAction((event) -> {
-                
-                String shortName = shortNameCustomField.getText();
-                Log newLog = new Log(currentTask, new Person(), 90, null);
-                currentTask.add(newLog);
-                App.refreshMainScene();
-                
-            });
+        
     }
 
     /**
