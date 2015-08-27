152d6838a34ec73c8dfa43d3bf312f3065431958
Codie Stevens <cvs20@uclive.ac.nz>
2015-08-27 14:24:47 +1200

    Added a prototype task visualisation bar

--- src/main/java/seng302/group2/scenes/information/project/sprint/SprintInfoTab.java
+++ src/main/java/seng302/group2/scenes/information/project/sprint/SprintInfoTab.java
@@ -5,8 +5,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import org.controlsfx.control.PopOver;
+import seng302.group2.scenes.control.PopOverTip;
 import seng302.group2.scenes.control.search.SearchableControl;
 import seng302.group2.scenes.control.search.SearchableTab;
 import seng302.group2.scenes.control.search.SearchableText;
@@ -115,6 +122,46 @@ public class SprintInfoTab extends SearchableTab {
 
         btnEdit.setOnAction((event) -> currentSprint.switchToInfoScene(true));
 
+
+
+        //Creates visualisation bar
+        GridPane visualGrid = new GridPane();
+
+        Rectangle red = new Rectangle();
+        Rectangle green = new Rectangle();
+        Rectangle blue = new Rectangle();
+
+        green.setWidth(120);
+        green.setHeight(25);
+        green.setArcWidth(15);
+        green.setArcHeight(15);
+        PopOverTip greenPO = new PopOverTip(green, new Text("Green"));
+        greenPO.setArrowLocation(PopOver.ArrowLocation.TOP_CENTER);
+
+        blue.setWidth(170);
+        blue.setHeight(25);
+        blue.setArcWidth(15);
+        blue.setArcHeight(15);
+        PopOverTip bluePO = new PopOverTip(blue, new Text("Blue"));
+        bluePO.setArrowLocation(PopOver.ArrowLocation.TOP_CENTER);
+
+        red.setWidth(230);
+        red.setHeight(25);
+        red.setArcWidth(15);
+        red.setArcHeight(15);
+        PopOverTip redPO = new PopOverTip(red, new Text("Red"));
+        redPO.setArrowLocation(PopOver.ArrowLocation.TOP_CENTER);
+
+        red.setFill(Color.RED);
+        green.setFill(Color.GREEN);
+        blue.setFill(Color.BLUE);
+
+        visualGrid.add(red, 0, 0, 1, 3);
+        visualGrid.add(blue, 0, 0, 1, 2);
+        visualGrid.add(green, 0, 0, 1, 1);
+
+        basicInfoPane.getChildren().add(visualGrid);
+
         Collections.addAll(searchControls, title);
     }
 
@@ -125,6 +172,8 @@ public class SprintInfoTab extends SearchableTab {
      */
     private VBox createStoryTitlePanes(Sprint currentSprint) {
         final VBox stackedStoryTitlePanes = new VBox();
+
+
         if (currentSprint.getStories().size() != 0) {
 
             List<Story> stories = new ArrayList<>();
