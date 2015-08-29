49eada58fb8abb108c5cad7a0a604182f0c004c2
Jordane Lew <jml168@uclive.ac.nz>
2015-08-29 16:17:47 +1200

    Story completeness bar altered to account for effort left in tasks that aren't started, and changed the list of tasks to include the correct lanes. Conditionally altered the GUI to ensure round and filled corners where necessary at different lengths of bars. (Messy, but works okay)

--- src/main/java/seng302/group2/scenes/control/chart/StoryCompletenessBar.java
+++ src/main/java/seng302/group2/scenes/control/chart/StoryCompletenessBar.java
@@ -29,21 +29,21 @@ public class StoryCompletenessBar extends Pane {
         ObservableList<Task> rest = FXCollections.observableArrayList();
 
         for (Task task : tasks) {
-            if (task.getState() == Task.TASKSTATE.DONE) {
+            if (task.getLane() == Task.TASKSTATE.DONE || task.getState() == Task.TASKSTATE.DEFERRED) {
                 done.add(task);
             }
-            else if (task.getState() == Task.TASKSTATE.IN_PROGRESS) {
-                inProgress.add(task);
+            else if (task.getLane() == Task.TASKSTATE.NOT_STARTED) {
+                rest.add(task);
             }
             else {
-                rest.add(task);
+                inProgress.add(task);
             }
         }
 
 
         double lengthGreen = getEffortSpent(done);
         double lengthBlue = getEffortSpent(inProgress);
-        double lengthRed = getEffortLeft(inProgress) + getEffortLeft(inProgress);
+        double lengthRed = getEffortLeft(inProgress) + getEffortLeft(rest);
 
         double overallLength = lengthBlue + lengthGreen + lengthRed;
 
@@ -95,24 +95,64 @@ public class StoryCompletenessBar extends Pane {
         redPO.setArrowLocation(PopOver.ArrowLocation.TOP_CENTER);
 
         red.setFill(ColorUtils.toColor(Task.TASKSTATE.BLOCKED.getColourString()));  // Red
-        redSquare.setFill(ColorUtils.toColor(Task.TASKSTATE.BLOCKED.getColourString()));
+        redSquare.setFill(red.getFill());
         green.setFill(ColorUtils.toColor(Task.TASKSTATE.DONE.getColourString()));  // Green
-        greenSquare.setFill(ColorUtils.toColor(Task.TASKSTATE.DONE.getColourString()));
+        greenSquare.setFill(green.getFill());
         blue.setFill(ColorUtils.toColor(Task.TASKSTATE.VERIFY.getColourString()));  // Blue
 
 
-        if (maxGreen > 15 && maxGreen < maxWidth - 15) {
-            visualGrid.add(greenBg, 0, 0);
+        if (maxBlue - maxGreen > 0.01) {
+            if (maxGreen > 15 && maxGreen < maxWidth - 15) {
+                visualGrid.add(greenBg, 0, 0);
+            }
+            if (maxGreen < 0.1) {
+                if (maxGreen + maxBlue > maxWidth - 0.01) {
+                    // round both
+                    blue.setArcHeight(15);
+                    blue.setArcWidth(15);
+                    visualGrid.add(blue, 1, 0);
+                }
+                else {
+                    // round left
+                    HBox blueBox = new HBox();
+                    Rectangle blueSquare = new Rectangle(maxBlue - maxGreen, blue.getHeight());
+                    blueSquare.setFill(blue.getFill());
+                    blue.setArcWidth(15);
+                    blue.setArcHeight(15);
+                    Rectangle transRec = new Rectangle(blue.getWidth()/2.0, blue.getHeight());
+                    transRec.setFill(Color.TRANSPARENT);
+                    blueBox.getChildren().add(transRec);
+                    blueBox.getChildren().add(blueSquare);
+                    visualGrid.add(blueBox, 1, 0);
+                    visualGrid.add(redSquare, 2, 0);
+                    visualGrid.add(blue, 1, 0);
+                }
+            }
+            else if (maxGreen + maxBlue > maxWidth - 0.01) {
+                // round right
+                Rectangle blueSquare = new Rectangle(maxBlue - 7.5, blue.getHeight());
+                blueSquare.setFill(blue.getFill());
+                blue.setArcWidth(15);
+                blue.setArcHeight(15);
+                visualGrid.add(blueSquare, 1, 0);
+                visualGrid.add(blue, 1, 0);
+            }
+            else {
+                // normal
+                visualGrid.add(blue, 1, 0);
+            }
         }
 
-        if (maxGreen + maxBlue > 15) {
+        if (maxGreen + maxBlue > 7 && !visualGrid.getChildren().contains(redSquare)) {
             visualGrid.add(redSquare, 2, 0);
         }
         visualGrid.add(green, 0, 0);
-        visualGrid.add(blue, 1, 0);
+
         visualGrid.add(red, 2, 0);
 
         this.getChildren().add(visualGrid);
+
+        System.out.println(visualGrid.getChildren());
 
     }
 
