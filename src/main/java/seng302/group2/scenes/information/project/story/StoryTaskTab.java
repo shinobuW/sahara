76009833c0bf60aef52d66302d03135ec3fbb7bc
Shinobu Williams <swi67@uclive.ac.nz>
2015-09-01 16:34:51 +1200

    Worked on displaying pop over when impediment cell is clicked. WIP.

--- src/main/java/seng302/group2/scenes/information/project/story/StoryTaskTab.java
+++ src/main/java/seng302/group2/scenes/information/project/story/StoryTaskTab.java
@@ -7,6 +7,7 @@ import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -425,7 +426,7 @@ public class StoryTaskTab extends SearchableTab {
                 setGraphic(null);
             }
             else {
-                this.popUp = createImpedimentsNode(getTask());
+                this.popUp = createImpedimentsNode(getTask(), this);
                 setGraphic(popUp);
             }
         }
@@ -442,7 +443,7 @@ public class StoryTaskTab extends SearchableTab {
     }
 
 
-    private Node createImpedimentsNode(Task task) {
+    private Node createImpedimentsNode(Task task, TableCell tableCell) {
         // Impediments icon
         ImageView warningImage;
         if (Task.getImpedingStates().contains(task.getState()) || !task.getImpediments().isEmpty()) {
@@ -476,86 +477,46 @@ public class StoryTaskTab extends SearchableTab {
             Tooltip.create("This task has no impediments or blockages", warningImage, 50);
         }
 
-//        warningImage.setOnMouseEntered(me -> {
-//            this.getScene().setCursor(Cursor.HAND); //Change cursor to hand
-//        });
-//        warningImage.setOnMouseExited(me -> {
-//            this.getScene().setCursor(Cursor.DEFAULT); //Change cursor to hand
-//        });
+        tableCell.setOnMouseEntered(me -> {
+                tableCell.setCursor(Cursor.HAND); //Change cursor to hand
+            });
+        tableCell.setOnMouseExited(me -> {
+                tableCell.setCursor(Cursor.DEFAULT); //Change cursor to hand
+            });
 
 
         PopOver impedimentPopOver = new PopOver();
         impedimentPopOver.setDetachedTitle(task.getShortName() + "'s Impediments");
-        SortedSet<Task.TASKSTATE> availableStatuses = new TreeSet<>();
-        try {
-            for (Task.TASKSTATE state : Task.getImpedingStates()) {
-                availableStatuses.add(state);
-            }
-        }
-        catch (NullPointerException ex) {
-        }
-
-        // Leave the combobox without a type so we can add a blank '(none)' without having to create a new task state
-        ComboBox impedimentCombo = new ComboBox<>();
-        impedimentCombo.getItems().add("(none)");
-        impedimentCombo.getItems().addAll(availableStatuses);
-        // Make default selection
-        if (impedimentCombo.getItems().contains(task.getState())) {
-            impedimentCombo.getSelectionModel().select(task.getState());
-        }
-        else {
-            impedimentCombo.getSelectionModel().select("(none)");
-        }
-        warningImage.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-                if (impedimentPopOver.isShowing()) {
-                    impedimentPopOver.hide();
-                }
-                else {
-                    impedimentPopOver.show(warningImage);
-                }
-                event.consume();
+        tableCell.setOnMouseClicked(me -> {
+                impedimentPopOver.show(tableCell);
             });
 
         VBox impedimentsVBox = new VBox(4);
+        impedimentsVBox.setPadding(new Insets(8, 8, 8, 8));
         SearchableText impedimentsLabel = new SearchableText("Impediments: ");
-        TextArea impedimentsTextArea = new TextArea(task.getImpediments());
-        impedimentsTextArea.setPrefSize(240, 80);
-        impedimentsVBox.getChildren().addAll(impedimentsLabel, impedimentsTextArea);
-
-        Button impedimentSaveButton = new Button("Save Impediments");
-        impedimentSaveButton.setAlignment(Pos.CENTER_RIGHT);
-        impedimentSaveButton.setOnAction(event -> {
-                Task.TASKSTATE selectedState = null;
-                Object selectedObject = impedimentCombo.getSelectionModel().getSelectedItem();
-                if (selectedObject == null || !selectedObject.toString().equals("(none)")) {
-                    selectedState = (Task.TASKSTATE) selectedObject;
-                }
-                task.editImpedimentState(selectedState, impedimentsTextArea.getText());
-                impedimentPopOver.hide();
-
-        //            this.getChildren().clear();
-        //            this.getChildren().add(construct());
-            });
+        Label impediments = new Label(task.getImpediments());
+//        impedimentsTextArea.setPrefSize(240, 80);
+        impedimentsVBox.getChildren().addAll(impedimentsLabel, impediments);
 
         SearchableText statusLabel = new SearchableText("Status: ");
         statusLabel.setTextAlignment(TextAlignment.LEFT);
         HBox impedimentComboLabel = new HBox(8);
         impedimentComboLabel.setAlignment(Pos.CENTER_RIGHT);
         impedimentComboLabel.getChildren().addAll(
-                statusLabel,
-                impedimentCombo
+                statusLabel
+
         );
 
 
-        VBox impedimentChangeNode = new VBox(8);
-        impedimentChangeNode.setAlignment(Pos.CENTER_RIGHT);
-        impedimentChangeNode.setPadding(new Insets(8, 8, 8, 8));
-        impedimentChangeNode.getChildren().addAll(
-                impedimentComboLabel,
-                impedimentsVBox,
-                impedimentSaveButton
-        );
-        impedimentPopOver.setContentNode(impedimentChangeNode);
+//        VBox impedimentsVBox = new VBox(8);
+//        impedimentsVBox.setAlignment(Pos.CENTER_RIGHT);
+//        impedimentsVBox.setPadding(new Insets(8, 8, 8, 8));
+//        impedimentsVBox.getChildren().addAll(
+//                impedimentComboLabel
+//
+//
+//        );
+        impedimentPopOver.setContentNode(impedimentsVBox);
 
         return warningImage;
     }
