67500cb04785f4b5d93da4b16e5b23d0c0e6806e
Codie Stevens <cvs20@uclive.ac.nz>
2015-09-06 13:40:09 +1200

    Created SearchableRadioButton class, changed the documentation in SearchableCheckBox and added GUI elements to SprintTaskStatusTab. Such as SearchableRadioButtons, SearchableText and Titles, and a combo box filled with filter options.

---
+++ src/main/java/seng302/group2/scenes/control/search/SearchableRadioButton.java
@@ -0,0 +1,84 @@
+package seng302.group2.scenes.control.search;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.Collection;
+
+/**
+ * A class to display a searchable radio button.
+ * Created by cvs20 on 6/09/15.
+ */
+public class SearchableRadioButton extends VBox implements SearchableControl {
+    private SearchableText rbLabel = new SearchableText();
+    private RadioButton rb = new RadioButton();
+
+    /**
+     * Constructor for a SearchableRadioButton that takes a label (as a String).
+     * @param label string used for the RadioButton label
+     */
+    public SearchableRadioButton(String label) {
+        HBox box = new HBox();
+        box.setSpacing(2);
+        box.setPrefWidth(165);
+        box.setAlignment(Pos.CENTER_LEFT);
+        box.spacingProperty().setValue(0);
+
+        rbLabel.setPadding(new Insets(0, 85, 0, 0));
+        rbLabel.setText(label);
+        rbLabel.setStyle("-fx-font-weight: bold");
+
+        box.getChildren().addAll(rb, rbLabel);
+
+        this.getChildren().add(box);
+
+    }
+
+    /**
+     * Constructor for a SearchableRadioButton that takes a label (as a String) and the searchableControls.
+     * @param label string used for the RadioButton label
+     * @param searchableControls A collection of searchable controls to add this control to
+     */
+    public SearchableRadioButton(String label, Collection<SearchableControl> searchableControls) {
+        HBox box = new HBox();
+        box.setPrefWidth(165);
+        box.setAlignment(Pos.CENTER_LEFT);
+        box.spacingProperty().setValue(0);
+
+        rbLabel.setPadding(new Insets(0, 85, 0, 0));
+        rbLabel.setText(label);
+        rbLabel.setStyle("-fx-font-weight: bold");
+        box.getChildren().addAll(rb, rbLabel);
+
+        this.getChildren().add(box);
+        searchableControls.add(this);
+    }
+
+    /**
+     * Gets the RadioButton out of this class.
+     * @return the RadioButton.
+     */
+    public RadioButton getRadioButton() {
+        return this.rb;
+    }
+
+    /**
+     * Queries the contents of the RadioButton label.
+     *
+     * @param query The search string to query the list contents with
+     * @return If a match was found for the query
+     */
+    @Override
+    public boolean query(String query) {
+        return rbLabel.query(query);
+    }
+
+    //TODO Advanced search query for this class.
+    @Override
+    public boolean advancedQuery(String query) {
+        return false;
+    }
+}
--- src/main/java/seng302/group2/scenes/information/project/sprint/SprintTaskStatusTab.java
+++ src/main/java/seng302/group2/scenes/information/project/sprint/SprintTaskStatusTab.java
@@ -1,19 +1,25 @@
 package seng302.group2.scenes.information.project.sprint;
 
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import seng302.group2.scenes.control.search.SearchableControl;
-import seng302.group2.scenes.control.search.SearchableTab;
-import seng302.group2.scenes.control.search.SearchableText;
-import seng302.group2.scenes.control.search.SearchableTitle;
+import seng302.group2.scenes.control.CustomComboBox;
+import seng302.group2.scenes.control.search.*;
 import seng302.group2.workspace.project.sprint.Sprint;
+import seng302.group2.workspace.team.Team;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+
+import static javafx.collections.FXCollections.observableArrayList;
 
 /**
  * Task Visualisation Tab
@@ -40,13 +46,45 @@ public class SprintTaskStatusTab extends SearchableTab {
         this.setContent(wrapper);
 
         SearchableText title = new SearchableTitle("Task Visualisation", searchControls);
+        SearchableText groupBy = new SearchableText("Group By: ", searchControls);
+        groupBy.setStyle("-fx-font-weight: bold");
+
+        ToggleGroup group = new ToggleGroup();
+        SearchableRadioButton status = new SearchableRadioButton("Status", searchControls);
+        SearchableRadioButton story = new SearchableRadioButton("Story", searchControls);
+        status.getRadioButton().setToggleGroup(group);
+        status.getRadioButton().setSelected(true);
+        story.getRadioButton().setToggleGroup(group);
+
+        HBox buttonBox = new HBox(10);
+        buttonBox.getChildren().addAll(groupBy, status, story);
+
+        CustomComboBox filterBox = new CustomComboBox("Filter by: ", searchControls);
+
+        String unassigned = new String("Unassigned");
+        String uncompleted = new String("Uncompleted");
+        String all = new String("All Tasks");
+
+        filterBox.addToComboBox(unassigned);
+        filterBox.addToComboBox(uncompleted);
+        filterBox.addToComboBox(all);
+
+        filterBox.setValue(unassigned);
+
+
 
         basicInfoPane.getChildren().addAll(
-                title
+                title,
+                buttonBox,
+                filterBox
         );
 
         Collections.addAll(searchControls,
-                title
+                title,
+                status,
+                story,
+                filterBox
+
         );
     }
 
