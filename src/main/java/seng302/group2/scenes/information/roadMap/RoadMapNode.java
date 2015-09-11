0b4360c34f3535cf0b97ccd5fa3b435a759d7b3a
Cameron Williams <Darzolak@hotmail.com>
2015-09-12 00:00:25 +1200

    Formatting on the Roadmap nodes and adding double click functionality to navigate to the relevant scenes.

--- src/main/java/seng302/group2/scenes/information/roadMap/RoadMapNode.java
+++ src/main/java/seng302/group2/scenes/information/roadMap/RoadMapNode.java
@@ -9,13 +9,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import javafx.scene.Node;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import org.controlsfx.control.spreadsheet.Grid;
+import seng302.group2.App;
 import seng302.group2.Global;
+import seng302.group2.scenes.MainPane;
 import seng302.group2.scenes.control.CustomInfoLabel;
+import seng302.group2.scenes.control.TrackedTabPane;
 import seng302.group2.scenes.control.search.SearchType;
 import seng302.group2.scenes.control.search.SearchableControl;
+import seng302.group2.scenes.control.search.SearchableTab;
 import seng302.group2.scenes.control.search.SearchableText;
+import seng302.group2.workspace.SaharaItem;
 import seng302.group2.workspace.project.Project;
 import seng302.group2.workspace.project.release.Release;
 import seng302.group2.workspace.project.sprint.Sprint;
@@ -51,18 +59,34 @@ public class RoadMapNode extends HBox implements SearchableControl {
     
     private Node createReleaseNode(Release release) {
         VBox releaseNode = new VBox();
+        HBox releaseContent = new HBox();
+        HBox releaseChildren = new HBox();
+        
         SearchableText shortNameField = new SearchableText(release.getShortName());
-        releaseNode.getChildren().addAll(
-                shortNameField
-        );
+        
+        shortNameField.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2) {
+                    App.mainPane.selectItem(release);
+                    release.switchToInfoScene();
+                }
+                event.consume();
+            });
+        
+        
         
         for (Project proj : Global.currentWorkspace.getProjects()) {
             for (Sprint sprint : proj.getSprints()) {
                 if (sprint.getRelease().equals(release)) {
-                    releaseNode.getChildren().add(createSprintNode(sprint));
+                    releaseChildren.getChildren().add(createSprintNode(sprint));
                 }
             }
         }
+        
+        releaseContent.getChildren().add(shortNameField);
+        releaseNode.getChildren().addAll(
+                releaseContent,
+                releaseChildren
+            );
 
         // Add items to pane & search collection
         Collections.addAll(searchControls,
@@ -74,7 +98,20 @@ public class RoadMapNode extends HBox implements SearchableControl {
     
     private Node createSprintNode(Sprint sprint) {
         VBox sprintNode = new VBox();
+        HBox sprintContent = new HBox();
+        GridPane sprintChildren = new GridPane();
+        int xCounter = 0;
+        int yCounter = 0;
+        
         SearchableText shortNameField = new SearchableText(sprint.getGoal());
+        
+        shortNameField.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2) {
+                    App.mainPane.selectItem(sprint);
+                    sprint.switchToInfoScene();
+                }
+                event.consume();
+            });
         
         sprintNode.getChildren().addAll(
                 shortNameField
@@ -82,12 +119,22 @@ public class RoadMapNode extends HBox implements SearchableControl {
         
         List<Node> releaseNodeList = new ArrayList<>();
         for (Story story : sprint.getStories()) {
-            sprintNode.getChildren().add(createStoryNode(story));
+            sprintChildren.add(createStoryNode(story), xCounter, yCounter);
+            xCounter++;
+            if (xCounter == 3) {
+                yCounter++;
+                xCounter = 0;
+            }
         }
 
+        sprintContent.getChildren().addAll(shortNameField);
+        sprintNode.getChildren().addAll(
+                sprintContent,
+                sprintChildren
+        );
         // Add items to pane & search collection
         Collections.addAll(searchControls,
-                shortNameField  
+                shortNameField
         );
         
         return sprintNode;
@@ -96,6 +143,14 @@ public class RoadMapNode extends HBox implements SearchableControl {
     private Node createStoryNode(Story story) {
         VBox storyNode = new VBox();
         SearchableText shortNameField = new SearchableText(story.getShortName());
+        
+        shortNameField.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2) {
+                    App.mainPane.selectItem(story);
+                    story.switchToInfoScene();
+                }
+                event.consume();
+            });
         
         
         storyNode.getChildren().addAll(
