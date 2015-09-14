887af0739cb289bd745c56ecd1720be1c9e09608
jml168 <jml168@uclive.ac.nz>
2015-09-14 18:04:07 +1200

    Create launcher .sh for Linux

---
+++ create_launcher.sh
@@ -0,0 +1,1 @@
+javapackager -deploy -native all -outdir packages -outfile Sahara -srcdir target -srcfiles project-2-1.0-SNAPSHOT-launcher.jar -appclass seng302.group2.App -name "Sahara" -title "Sahara"
