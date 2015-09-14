653a152a12837be6e0fc65fcb6aeb0d0a7e5d750
Shinobu Williams <swi67@uclive.ac.nz>
2015-09-14 23:57:11 +1200

    Created a new log tab for sprint. WIP.

--- src/main/java/seng302/group2/scenes/information/project/sprint/SprintLogTab.java
+++ src/main/java/seng302/group2/scenes/information/project/sprint/SprintLogTab.java
@@ -37,42 +37,42 @@ public class SprintLogTab extends SearchableTab {
         this.setContent(wrapper);
 
         Platform.runLater(() -> {
-            SearchableText title = new SearchableTitle(sprint.getLongName() + " Logging Effort");
+                SearchableText title = new SearchableTitle(sprint.getLongName() + " Logging Effort");
 
-            final Separator separator = new Separator();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                final Separator separator = new Separator();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 
-//            loggingPane.getChildren().addAll(
-//                    title,
-//                    sprintName,
-//                    sprintGoal,
-//                    sprintStart,
-//                    sprintEnd,
-//                    desc,
-//                    separator,
-//                    team,
-//                    project,
-//                    release,
-//                    stories
-//
-//            );
-//
-//            Collections.addAll(searchControls,
-//                    title,
-//                    sprintName,
-//                    sprintGoal,
-//                    sprintStart,
-//                    sprintEnd,
-//                    desc,
-//                    team,
-//                    project,
-//                    release,
-//                    stories
-//            );
+    //            loggingPane.getChildren().addAll(
+    //                    title,
+    //                    sprintName,
+    //                    sprintGoal,
+    //                    sprintStart,
+    //                    sprintEnd,
+    //                    desc,
+    //                    separator,
+    //                    team,
+    //                    project,
+    //                    release,
+    //                    stories
+    //
+    //            );
+    //
+    //            Collections.addAll(searchControls,
+    //                    title,
+    //                    sprintName,
+    //                    sprintGoal,
+    //                    sprintStart,
+    //                    sprintEnd,
+    //                    desc,
+    //                    team,
+    //                    project,
+    //                    release,
+    //                    stories
+    //            );
 
-//            loggingPane.getChildren().addAll((currentSprint), btnEdit);
-        });
+    //            loggingPane.getChildren().addAll((currentSprint), btnEdit);
+            });
 
     }
 
