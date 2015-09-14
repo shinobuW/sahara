f5898d7be5ae7146567e1c6f004c10a6dde73129
jml168 <jml168@uclive.ac.nz>
2015-09-14 19:14:48 +1200

    Launcher for linux updated, working

--- create_launcher.sh
+++ create_launcher.sh
@@ -1,1 +1,2 @@
-javapackager -deploy -native all -outdir packages -outfile Sahara -srcdir target -srcfiles project-2-1.0-SNAPSHOT-launcher.jar -appclass seng302.group2.App -name "Sahara" -title "Sahara"
+export JAVA_HOME="/home/cosc/student/jml168/Documents/jdk1.8.0_51";
+javapackager -deploy -native image -outdir packages -outfile Sahara -srcdir target -srcfiles project-2-1.0-SNAPSHOT-launcher.jar -appclass seng302.group2.App -name "Sahara" -title "Sahara"
