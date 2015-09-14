acfa25d96cee608ef844090873955a2a084a5200
Shinobu Williams <swi67@uclive.ac.nz>
2015-09-14 17:47:31 +1200

    Created a script for creating a launcher for Sahara

---
+++ create_launcher.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Java\jdk1.8.0_40\bin\javafxpackager" -deploy -native all -outdir
+packages -outfile Sahara -srcdir . -srcfiles project-2-1.0-SNAPSHOT-launcher.jar -
+appclass seng302.group2.App -name "Sahara" -title "Sahara"
