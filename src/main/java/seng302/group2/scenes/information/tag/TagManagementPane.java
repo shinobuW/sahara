07776dfed05249ecc32e87fc714139cd96053011
jml168 <jml168@uclive.ac.nz>
2015-09-15 16:56:33 +1200

    Updated and populated the tag management pane with controls

--- src/main/java/seng302/group2/scenes/information/tag/TagManagementPane.java
+++ src/main/java/seng302/group2/scenes/information/tag/TagManagementPane.java
@@ -3,16 +3,20 @@ package seng302.group2.scenes.information.tag;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import org.controlsfx.control.PopOver;
 import seng302.group2.Global;
+import seng302.group2.scenes.control.RequiredField;
 import seng302.group2.scenes.control.search.SearchableControl;
 import seng302.group2.scenes.control.search.SearchableListView;
 import seng302.group2.workspace.tag.Tag;
 
+import java.awt.*;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -56,5 +60,23 @@ public class TagManagementPane extends SplitPane {
 
     private void constructDetail() {
 
+        // Find the selected tag and create its tag node
+        Tag selectedTag = tagListView.getSelectionModel().getSelectedItem();
+        TagCellNode cellNode = new TagCellNode(selectedTag);
+
+        // Create the editable option controls
+        RequiredField tagNameField = new RequiredField("Tag Name", searchControls);
+        tagNameField.setText("#" + selectedTag.getName());
+
+        ColorPicker colorPicker = new ColorPicker(selectedTag.getColor());
+        colorPicker.setOnAction(event -> {
+                Color newColor = colorPicker.getValue();
+                System.out.println("New Color's RGB = " + newColor.getRed() + " "
+                        + newColor.getGreen() + " " + newColor.getBlue());
+        });
+
+        // Group all of the controls into a details pane
+        VBox detailsPane = new VBox(8);
+        detailsPane.getChildren().addAll(cellNode, tagNameField, colorPicker);
     }
 }
