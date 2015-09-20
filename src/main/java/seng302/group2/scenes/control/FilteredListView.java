ffaf362642c3a66a6f698911f5726b58decf04f4
Cameron Williams <crw73@uclive.ac.nz>
2015-09-20 20:47:44 +1200

    Created Filtered Listview.

---
+++ src/main/java/seng302/group2/scenes/control/FilteredListView.java
@@ -0,0 +1,84 @@
+package seng302.group2.scenes.control;
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import seng302.group2.scenes.control.search.SearchType;
+import seng302.group2.scenes.control.search.SearchableControl;
+import seng302.group2.scenes.control.search.SearchableListView;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import static javafx.collections.FXCollections.observableArrayList;
+
+/**
+ * Created by crw73 on 20/09/15.
+ */
+public class FilteredListView<T> extends VBox implements SearchableControl {
+
+    private Set<SearchableControl> searchControls = new HashSet<>();
+    private ObservableList<T> sortedData = observableArrayList();
+    private TextField inputText = new TextField();
+    private SearchableListView<T> listView;
+
+
+
+    public FilteredListView(ObservableList<T> data) {
+        inputText.setPromptText("Search list...");
+        listView = new SearchableListView(data);
+        inputText.setOnKeyReleased(event -> {
+            sortedData.clear();
+            for (T item : data) {
+                if (item.toString().toLowerCase().contains(inputText.getText().toLowerCase())) {
+                    sortedData.add(item);
+                }
+            }
+            listView.setItems(sortedData);
+        });
+
+        this.getChildren().addAll(
+                inputText,
+                listView
+        );
+
+        Collections.addAll(searchControls, listView);
+    }
+
+    public SearchableListView<T> getListView() {
+        return listView;
+    }
+
+    @Override
+    /**
+     * Queries the HBox to find any elements containing the given query string. If found inside
+     * the HBox, the matching text will be highlighted.
+     * @param query The query string to search
+     * @return Whether any elements inside the HBox were found to contain the query string
+     */
+    public boolean query(String query) {
+        boolean found = false;
+
+        for (SearchableControl control : searchControls) {
+            if (control.query(query)) {
+                found = true;
+            }
+        }
+        return found;
+    }
+
+    @Override
+    /**
+     * Queries the HBox to find any elements containing the given query string. If found inside
+     * the HBox, the matching text will be highlighted.
+     * @param query The query string to search
+     * @return Whether any elements inside the HBox were found to contain the query string
+     */
+    public int advancedQuery(String query, SearchType searchType) {
+        return 0;
+    }
+}
