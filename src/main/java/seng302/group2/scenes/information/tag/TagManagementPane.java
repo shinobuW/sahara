ee3754ab02aa1c3abb7785c12f759c3601fd3d93
drm127 <drm127@uclive.ac.nz>
2015-09-21 13:04:02 +1200

    Fixed tag management pane not displaying newly created tags in the list view after removing a search term from the list view filter box. Also made list view select newly created tags upon creation.

--- src/main/java/seng302/group2/scenes/information/tag/TagManagementPane.java
+++ src/main/java/seng302/group2/scenes/information/tag/TagManagementPane.java
@@ -156,9 +156,9 @@ public class TagManagementPane extends SplitPane {
             Tag newTag = new Tag(newTagField.getText());
             Global.currentWorkspace.add(newTag);
 
-            tagListView.getSelectionModel().select(newTag);
-
             newTagField.clear();
+            this.constructList();
+            tagListView.getSelectionModel().select(newTag);
         });
 
         newTagField.textProperty().addListener((observable, oldValue, newValue) -> {
